2dd83eb6e27a805954f2a1be3ec8dd2944b22177
Mine <air1345@gmail.com>
2025-09-16 23:29:23 +0500

    Update wpl.js

--- wpl.js
+++ wpl.js
@@ -1,1 +1,11 @@
+const users = [
+  { username: "Leeli", name: "Leeli Lounge", password: "12345", rcNo: "1",  WP: "Leeli" },
+  { username: "Koveli", name: "Koveli Lounge", password: "12345", rcNo: "2", WP: "Koveli" }
+
+];
+
+
+
+
+
 
